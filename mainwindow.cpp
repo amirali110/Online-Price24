5c9afe3e733da2cc989410c305694b0ff9844e58
Amirali <37404187+amirali110@users.noreply.github.com>
2021-06-24 22:10:28 +0430

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -143,7 +143,7 @@ QVariantList MainWindow::forex(){
 
     GET symbol7("https://twelve-data1.p.rapidapi.com/price?symbol=CAD/USD");
 
-    doc=QJsonDocument::fromJson(symbol7.Get_Reply("x-rapidapi-key","91c969a8e9msh8b47ffbad7457eep11a88ajsnc07f9fe9120e"));
+    doc=QJsonDocument::fromJson(symbol7.Get_Reply("x-rapidapi-key",api_key));
     obj=doc.object();
     list.append(obj.value("price"));
 
